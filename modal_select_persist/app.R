f028820dbfeb56e36a4815c671c14bfe3ed64e93
Adam Black <ablack3@gmail.com>
2018-04-12 14:49:19 -0400

    added app example of a modal select box that has persistant selected values

---
+++ modal_select_persist/app.R
@@ -0,0 +1,23 @@
+library(shiny)
+options(shiny.reactlog=T) 
+
+ui <- fluidPage(
+     actionButton("button", "Button"),
+     verbatimTextOutput("selected")
+     )
+
+server <- function(input, output) {
+     # selected_letters <- reactiveValues()
+     my_input <- reactive(selectInput("letters", "letters", selected = input[["letters"]], choices = letters, multiple = T))
+     
+     observeEvent(input$button, {
+               # showModal(modalDialog(selectInput("letters", "letters", selected = selected_letters, choices = letters, multiple = T)))
+               showModal(modalDialog(my_input()))
+               # selected_letters <- input$letters
+              })
+
+     output$selected <- renderPrint(input$letters)
+}
+
+shinyApp(ui = ui, server = server)
+
