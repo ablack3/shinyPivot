f152b515e3f781592d9295d1de86c885ac4b510d
Adam Black <ablack3@gmail.com>
2018-04-13 14:44:52 -0400

    Two connected order inputs with persistant filtering.

--- shiny.oninputchange-example.R
+++ shiny.oninputchange-example.R
@@ -6,34 +6,32 @@ library(tidyverse)
 df <- starwars %>% 
      select_if(is.character)
 
+pivot_vars <- df %>% 
+     summarise_all(n_distinct) %>% 
+     collect() %>% 
+     gather("field", "n_levels") %>% 
+     mutate(levels = map(field, ~pull(distinct(select(df, .))))) 
 
-ui <- shinyUI( bootstrapPage(
+
+ui <- shinyUI(bootstrapPage(
      verbatimTextOutput("results"),
      verbatimTextOutput("results2"),
-     orderInput("source_vars", "Variables", items = names(df)),
+     orderInput("source_vars", "Variables", items = names(df), connect = "col_vars"),
+     orderInput("col_vars", "Columns", items = NULL, placeholder = "Drag variables here", connect = c("source_vars")),
      includeScript("mycode.js")
 ))
 
 
 server <- shinyServer(function(input, output, session) {
-     output$results = renderPrint({input$varname})
-     # output$results2 = renderPrint({
-     #      req(pivot_vars$filtered[[varnum()]]())
-     #      pivot_vars$filtered[[varnum()]]()
-     # })
+
+     pivot_vars <- pivot_vars %>% 
+          mutate(select_input = map2(field, levels, 
+                ~reactive(selectInput(.x, label = .x, choices = c("All levels" = "", .y), selected = input[[.x]], multiple = T)))) %>% 
+          mutate(filtered = map(field, ~reactive({length(input[[.x]]) > 0})))
      
      varnum <- reactive(match(input$varname, pivot_vars$field))
-     
-     pivot_vars <- df %>% 
-          summarise_all(n_distinct) %>% 
-          collect() %>% 
-          gather("field", "n_levels") %>% 
-          mutate(levels = map(pivot_vars$field, ~pull(distinct(select(df, .))))) %>% 
-          mutate(select_input = map2(pivot_vars$field, pivot_vars$levels, 
-                ~reactive(selectInput(.x, label = .x, choices = c("All levels" = "", .y), selected = input[[.x]], multiple = T)))) %>% 
-          mutate(filtered = map(pivot_vars$field, ~reactive({length(input[[.x]]) > 0})))
-     
-     observeEvent(input$click_counter, {
+
+          observeEvent(input$click_counter, {
           reactive_select <- pivot_vars[1, "select_input"]
                showModal(modalDialog(
                     title = "Filter",
@@ -43,6 +41,13 @@ server <- shinyServer(function(input, output, session) {
           
           # color = rgb(runif(1), runif(1), runif(1))
           # session$sendCustomMessage(type = "myCallbackHandler", color)
+     
+     output$results = renderPrint({input$varname})
+     # output$results2 = renderPrint({
+     #      req(pivot_vars$filtered[[varnum()]]())
+     #      pivot_vars$filtered[[varnum()]]()
+     # })
+     
      })
 })
 
