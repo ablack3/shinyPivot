525ac499ea6a0a67af26df44b444563b874718f8
Adam Black <ablack3@gmail.com>
2018-04-18 13:12:06 -0400

    modularized pivot app working

--- modularized_app.R
+++ modularized_app.R
@@ -5,10 +5,21 @@ source("shiny_pivot_module.R")
 
 options(shiny.reactlog=T) # command f3 + right arrow
 
+# local table
 df <- starwars %>% 
      select_if(is.character)
 
+
+
+# using a database
+# con <- DBI::dbConnect(RSQLite::SQLite(), path = ":memory:")
+# con <-  DBI::dbConnect(RPostgreSQL::PostgreSQL(), user="adamblack", password="",
+#                        host="localhost", port=5432, dbname="test")
+# copy_to(con, df, "star_wars")
+# df <- tbl(con, "star_wars")
+
 pivot_vars <- get_pivot_vars(df)
+
 
 
 ui <- fluidPage(title = "R pivot table", 
@@ -17,7 +28,7 @@ ui <- fluidPage(title = "R pivot table",
 
 
 server <- function(input, output, session){
-     callModule(shiny_pivot_module, "id", df = df, pivot_vars = pivot_vars)     
+     callModule(shiny_pivot_module, "id", ns_id = "id", df = df, pivot_vars = pivot_vars)     
 } # end server
 
 shinyApp(ui = ui, server = server)
--- shiny_pivot_module.R
+++ shiny_pivot_module.R
@@ -21,21 +21,22 @@ shiny_pivot_module_UI <- function(id, pivot_vars){
                      $(document).ready(function() {{
      
                      const b = document.querySelectorAll("#{ns("source_vars")} div");
-                     //console.log(b);
-                     //console.log("b.length is " + b.length)
+                     console.log("#{ns("source_vars")} div")
+                     console.log(b);
+                     console.log("b.length is " + b.length)
                      var click_counter = 0;
                      
                      function sendDataToShiny(clicked_button) {{
                           click_counter++;
                           var val = clicked_button.getAttribute("data-value");
-                         console.log("click_counter is " + click_counter);
+                          console.log("you clicked " + val + " and click_counter is " + click_counter);
                           Shiny.onInputChange({nsq("varname")}, val);
                           Shiny.onInputChange({nsq("click_counter")}, click_counter);
                      }}
                      
                      for (var i = 0; i < b.length; i++) {{
                           b[i].addEventListener("click", function(){{
-                              console.log("you clicked " + this.getAttribute("data-value"));
+                              //console.log("you clicked " + this.getAttribute("data-value"));
                               sendDataToShiny(this);
                                    
                          }}, false);
@@ -74,65 +75,68 @@ shiny_pivot_module_UI <- function(id, pivot_vars){
      )
 }
 
-shiny_pivot_module <- function(input, output, session, df, pivot_vars, record_limit = 1e6){
+shiny_pivot_module <- function(input, output, session, ns_id, df, pivot_vars, record_limit = 1e6){
      
+     ns <- NS(ns_id)
      # add reactive values to the pivot vars tibble in a list column. 
      # One select input and one T/F filtered indicator per pivot variable.
+     # Need to add namespace to any newly created input ids. Use ns() when defining new input id
      pivot_vars <- pivot_vars %>%
           mutate(select_input = map2(field, levels,
                ~reactive(selectInput(ns(.x), label = .x, choices = c("All levels" = "", .y), selected = input[[.x]], multiple = T)))) %>%
           mutate(filtered = purrr::map(field, ~reactive({length(input[[.x]]) > 0})))
 
+     print(pivot_vars)
      # which variable was clicked? (represented as a number from 1 to number of pivot vars)
      varnum <- reactive(match(input$varname, pivot_vars$field))
-     
-     # open dialog box when clicked
+
+     # # open dialog box when clicked
      observeEvent(input$click_counter, {
           showModal(modalDialog(easyClose = T, title = "Filter", pivot_vars$select_input[[varnum()]]()))
      })
-     
-     
+
+
      filter_expr <- reactive({
           # T/F indicators to select rows of filtered variables
           selector <- map_lgl(pivot_vars$filtered, ~.())
-          
+
           # initially no variables will be filtered
           if(all(!selector)) return(NULL)
-          
-          exp_builder <- pivot_vars %>% 
-               filter(selector) %>% 
-               mutate(selected_levels = map(field, ~input[[.]])) %>% 
-               mutate(selected_levels = map(selected_levels, ~paste(.))) %>% 
+
+          exp_builder <- pivot_vars %>%
+               filter(selector) %>%
+               mutate(selected_levels = map(field, ~input[[.]])) %>%
+               mutate(selected_levels = map(selected_levels, ~paste(.))) %>%
                select(field, selected_levels)
-          
+
           map2(exp_builder$field, exp_builder$selected_levels, ~rlang::expr(!!as.name(.x) %in% !!.y)) %>%
                reduce(function(a,b) rlang::expr(!!a & !!b))
      })
-     
+
      # maybe use event reactive and update button
      filtered_data <- reactive({
           grp_vars <- rlang::parse_quosures(paste0(c(input$row_vars_order, input$col_vars_order), collapse = ";"))
-          df %>% 
+          df %>%
           {if(!is.null(filter_expr)) filter(., !!!filter_expr()) else .} %>% # conditional pipe
-               group_by(!!!grp_vars) %>% 
-               summarise(n = n()) %>% 
+               group_by(!!!grp_vars) %>%
+               summarise(n = n()) %>%
                ungroup()
      })
-     
+
      # also need to add warning for exceeding row limit
      local_table <- reactive({
-          filtered_data() %>% 
-               filter(between(row_number(), 1, record_limit)) %>% 
-               collect() %>% 
-               mutate_if(~class(.) == "integer64", as.numeric) %>% 
+          filtered_data() %>%
+               filter(between(row_number(), 1, record_limit)) %>%
+               collect() %>%
+               mutate_if(~class(.) == "integer64", as.numeric) %>%
                {
                     if(length(input$col_vars_order) > 0 ){
-                         tidyr::unite(., "col_var", input$col_vars_order, sep = "_&_") %>% 
+                         tidyr::unite(., "col_var", input$col_vars_order, sep = "_&_") %>%
                               tidyr::spread(col_var, n, fill = 0)
                     } else .
                }
      })
-     
+
 
      # update button colors based on filtering
      observe({
@@ -146,7 +150,8 @@ shiny_pivot_module <- function(input, output, session, df, pivot_vars, record_li
      })
 
      output$table <- renderDataTable(local_table())
-     output$debug_text <- renderPrint(varnum())
+     # output$debug_text <- renderPrint(varnum())
+     output$debug_text <- renderPrint(NULL)
      output$download_data <- downloadHandler(
           filename = function() paste0("data_", Sys.Date(), ".csv"),
           content = function(file) readr::write_csv(local_table(), file),
