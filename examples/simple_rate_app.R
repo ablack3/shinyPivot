a8f44124c3d4ede5a9d67e0788a9f7085e702430
Adam Black <ablack3@gmail.com>
2019-07-25 21:58:17 -0400

    working simple example of pivot rate module before tree ui redesign

--- examples/simple_rate_app.R
+++ examples/simple_rate_app.R
@@ -1,47 +1,42 @@
-library(shiny)
-library(here)
-devtools::load_all()
 # options(shiny.reactlog=T) # command f3 + right arrow
-# source(here("dev/tree_ui/pivot_module_tree.R"))
+
+library(shiny)
+library(wakefield)
+library(dplyr)
+
 
 # local table
-library(dplyr)
-df1 <- starwars %>%
-     select_if(~is.character(.)|is.numeric(.))
+denom <- r_data_frame(10000, age(), gender(), year(x = 2015:2017), month(), elig = runif())
+numerator <- r_data_frame(1e5, age(), gender(), year(x = 2015:2017), month(), military(), internet_browser(), height(), income())
 
+# using a database
+con <- DBI::dbConnect(RSQLite::SQLite(), path = ":memory:")
 
-pivot_vars1 <- get_pivot_vars(df1) %>%
-        mutate(description = case_when(
-                field == "name" ~ "The name of the character",
-                field == "height" ~ "The height of the character",
-                T ~ field
-        )) %>%
-        mutate(group = case_when(
-                stringr::str_detect(field, "color") ~ "Color fields",
-                field %in% c("homeworld", "species", "name", "gender") ~ "Demographic fields",
-                T ~ "Other fields"
-        )) %>%
-        mutate(field_display_name = tools::toTitleCase(gsub("_", " ", field)))
+copy_to(con, denom, "denom")
+denom_db <- tbl(con, "denom")
 
+copy_to(con, numerator, "numerator")
+numerator_db <- tbl(con, "numerator")
 
-sum_vars <- c("height", "mass")
+pivot_vars1 <- numerator %>%
+     select(-Height, -Income) %>%
+     get_pivot_vars(max_levels = 1e7)
 
-ui <- fluidPage(title = "R pivot table",
+sum_vars <- c("Height", "Income")
+
+ui <- fluidPage(title = "R pivot rate table", #theme = shinythemes::shinytheme("superhero"),
                 tabsetPanel(
-                     tabPanel("Local pivot", pivot_tree_module_UI(id = "id1", pivot_vars = pivot_vars1, sum_vars = sum_vars))
+                     tabPanel("Local pivot",    pivot_rate_module_UI(id = "id1", pivot_vars = pivot_vars1, sum_vars = sum_vars)),
+                     tabPanel("Database pivot", pivot_rate_module_UI(id = "id2", pivot_vars = pivot_vars1, sum_vars = sum_vars))
                 )
 )
 
 server <- function(input, output, session){
-     callModule(pivot_tree_module, id = "id1", ns_id = "id1", df = df1, pivot_vars = pivot_vars1, record_limit = 20)
+     callModule(pivot_rate_module, id = "id1", ns_id = "id1", numer_df = numerator,    denom_df = denom,    pivot_vars = pivot_vars1, record_limit = 1e6)
+     callModule(pivot_rate_module, id = "id2", ns_id = "id2", numer_df = numerator_db, denom_df = denom_db, pivot_vars = pivot_vars1, record_limit = 1e6)
 } # end server
 
 shinyApp(ui = ui, server = server)
-
-# linked pivot table idea?
-
-
-
 
 
 
